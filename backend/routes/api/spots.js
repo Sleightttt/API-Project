38264c35590593104572b7d2bd430098381bf2eb
Brennan Cota <brennancota@yahoo.com>
2023-01-24 13:54:31 -0800

    finished implementing create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Sequelize = require("sequelize");
 const { Spot, SpotImage, Review } = require("../../db/models");
-const { requireAuth } = require("../../utils/auth");
+const { requireAuth, restoreUser } = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
   const spotts = await Spot.findAll({});
@@ -32,9 +32,60 @@ router.get("/", async (req, res) => {
   return res.json({ spots: spotsList });
 });
 
-router.get("/current", requireAuth, async (req, res) => {
-  console.log();
-  return res.json({ message: "hello" });
+router.get("/current", [requireAuth, restoreUser], async (req, res) => {
+  console.log(req.user.dataValues.id);
+
+  const Spots = await Spot.findAll({
+    where: {
+      ownerId: req.user.dataValues.id,
+    },
+  });
+  let spotsList = [];
+  for (let spot of Spots) {
+    let newSpot = spot.toJSON();
+
+    let previewImg = await SpotImage.findOne({
+      where: {
+        spotId: newSpot.id,
+        preview: true,
+      },
+    });
+    newSpot.previewImage = previewImg.url;
+
+    let avgRate = await Review.findAll({
+      where: {
+        spotId: spot.id,
+      },
+      attributes: [[Sequelize.fn("AVG", Sequelize.col("stars")), "stars"]],
+    });
+    newSpot.avgRating = avgRate[0].dataValues.stars;
+    spotsList.push(newSpot);
+  }
+
+  return res.json({ Spots: spotsList });
+});
+
+//create a spot
+
+router.post("/", [requireAuth, restoreUser], async (req, res, next) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+
+  const newSpot = await Spot.create({
+    ownerId: req.user.dataValues.id,
+    address: address,
+    city: city,
+    state: state,
+    country: country,
+    lat: lat,
+    lng: lng,
+    name: name,
+    description: description,
+    price: price,
+  });
+  const spotinfo = await Spot.findOne({ where: { id: newSpot.id } });
+
+  return res.json(spotinfo);
 });
 
 module.exports = router;
